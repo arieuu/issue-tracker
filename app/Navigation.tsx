576830e28f9060a806f967bea15991f7f160ec87
Pliskin <arieljanickcarvalho@gmail.com>
2024-01-22 11:18:05 -0100

    Finished 'setting up project' section.
    Styled active link

--- app/Navigation.tsx
+++ app/Navigation.tsx
@@ -1,8 +1,13 @@
+"use client";
 import React from 'react';
 import Link from 'next/link';
 import { AiFillBug } from "react-icons/ai";
+import { usePathname } from 'next/navigation';
+import classNames from 'classnames'; // Library to handle dynamic classes/classnames
 
 const Navigation = () => {
+    
+    const currentPath = usePathname();
 
     // Put all links in an array so we can style them all at once
 
@@ -16,9 +21,15 @@ const Navigation = () => {
         <Link href="/"> <AiFillBug /> </Link>
 
         <ul className='flex space-x-6'>
+
             { links.map((link) => {
-                return <li> <Link key={link.href} href={link.href} className='text-zinc-500 hover:text-zinc-800 transition-colors'> { link.label } </Link> </li>
+                return <li> <Link key={link.href} href={link.href} className={classNames({
+                    "text-zinc-900": link.href === currentPath,
+                    "text-zinc-500": link.href !== currentPath,
+                    "hover:text-zinc-800 transition-colors": true
+                })}> { link.label } </Link> </li>
             })}
+
         </ul>
         
     </nav>
