d06601af2e408dd44845c00c3d66e6a36c20d28b
Pliskin <arieljanickcarvalho@gmail.com>
2024-01-22 10:59:46 -0100

    Built the navbar

---
+++ app/Navigation.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Link from 'next/link';
+import { AiFillBug } from "react-icons/ai";
+
+const Navigation = () => {
+
+    // Put all links in an array so we can style them all at once
+
+    const links = [
+        { label: "Dashboard", href: "/" },
+        { label: "Issues", href: "/issues" }
+    ]
+
+    return (
+    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+        <Link href="/"> <AiFillBug /> </Link>
+
+        <ul className='flex space-x-6'>
+            { links.map((link) => {
+                return <li> <Link key={link.href} href={link.href} className='text-zinc-500 hover:text-zinc-800 transition-colors'> { link.label } </Link> </li>
+            })}
+        </ul>
+        
+    </nav>
+    )
+}
+
+export default Navigation
