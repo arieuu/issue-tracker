0ae9d704c667596635ab71b915ce35b91ae649bd
Pliskin <arieljanickcarvalho@gmail.com>
2024-01-23 11:03:45 -0100

    Showing issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,46 @@
-import { Button } from '@radix-ui/themes'
+import { Button, Table, TableColumnHeaderCell } from '@radix-ui/themes'
 import Link from 'next/link'
 import React from 'react'
+import prisma from '@/prisma/client'
 
-const IssuesPage = () => {
+const IssuesPage = async() => {
+
+  const issues = await prisma.issue.findMany();
+
   return (
-    <Button > <Link href="/issues/new"> New issue </Link> </Button>
+    <div>
+      <div className='mb-5'>
+        <Button > <Link href="/issues/new"> New issue </Link> </Button>
+      </div>
+
+      <Table.Root variant='surface'>
+        <Table.Header>
+          <Table.Row>
+            <TableColumnHeaderCell> Issue </TableColumnHeaderCell>
+            <TableColumnHeaderCell className='hidden md:table-cell'> Status </TableColumnHeaderCell>
+            <TableColumnHeaderCell className='hidden md:table-cell'> Created </TableColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {issues.map(issue => (
+
+            <Table.Row key={issue.id}>
+
+              <Table.Cell>
+                 { issue.title } 
+                 <div className='block md:hidden'> { issue.status } </div>
+              </Table.Cell>
+
+              <Table.Cell className='hidden md:table-cell'> { issue.status } </Table.Cell>
+              <Table.Cell className='hidden md:table-cell'> { issue.createdAt.toDateString() } </Table.Cell>
+            </Table.Row>
+
+          ))}
+        </Table.Body>
+
+      </Table.Root>
+    </div>
   )
 }
 
