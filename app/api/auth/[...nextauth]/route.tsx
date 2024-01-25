7c3447ce95661572045526b79208a987913103cb
Pliskin <arieljanickcarvalho@gmail.com>
2024-01-25 09:14:26 -0100

    Configured google provider

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,8 +1,18 @@
 
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
 
 const handler = NextAuth({
-  providers: []
+  providers: [
+    GoogleProvider({
+    
+    // The exclamation points at the end ensure the TS compiler that we do have those values (they're in the env file)
+
+    clientId: process.env.GOOGLE_CLIENT_ID!,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+  })
+  ]
 });
 
 export { handler as GET, handler as POST }
