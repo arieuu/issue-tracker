fb4d8e8ce9013a805ab332be01bfdddf265c1bb0
Pliskin <arieljanickcarvalho@gmail.com>
2024-01-26 21:51:02 -0100

    Assigning issue to users

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,11 +1,15 @@
 "use client";
-import { User } from '@prisma/client';
+import { Issue, User } from '@prisma/client';
 import { Select } from '@radix-ui/themes'
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { Skeleton } from "@/app/components";
 
-const AssigneeSelect = () => {
+interface Props {
+    issue: Issue
+}
+
+const AssigneeSelect = ({ issue }: Props) => {
 
     const { data: users, error, isLoading } = useQuery<User[]>({
         queryKey: ["users"],
@@ -19,11 +23,22 @@ const AssigneeSelect = () => {
     if(error) return null;
 
     return (
-        <Select.Root>
+        <Select.Root defaultValue={ issue.assignedToUserId || "" } onValueChange={(userId) => {
+
+            /** We don't need to await this call because we don't need to do anything after
+             * The select will display the selected user and the patch request can take it's time
+             * to register in the database
+             */
+
+            // Send the assigned user id if it exists or null if we are unassigning
+
+            axios.patch("/api/issues/" + issue.id, { assignedToUserId: userId || null });
+        }}>
             <Select.Trigger placeholder="Assign"/>
                 <Select.Content>
                     <Select.Group>
                         <Select.Label> Suggestion </Select.Label>
+                        <Select.Item value=""> Unassigned </Select.Item>
                         { users?.map(user => {
                             return <Select.Item key={user.id} value={user.id}> { user.name } </Select.Item>
                         })}
