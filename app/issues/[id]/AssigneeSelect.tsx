f36176ac86067727c1ad3d51f526bddb2c57d06c
Pliskin <arieljanickcarvalho@gmail.com>
2024-01-26 22:14:26 -0100

    Refactored assignee select component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,9 +1,9 @@
 "use client";
+import { Skeleton } from "@/app/components";
 import { Issue, User } from '@prisma/client';
-import { Select } from '@radix-ui/themes'
+import { Select } from '@radix-ui/themes';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import { Skeleton } from "@/app/components";
 import toast, { Toaster } from "react-hot-toast";
 
 interface Props {
@@ -12,33 +12,30 @@ interface Props {
 
 const AssigneeSelect = ({ issue }: Props) => {
 
-    const { data: users, error, isLoading } = useQuery<User[]>({
-        queryKey: ["users"],
-        queryFn: () => axios.get("/api/users").then(res => res.data),
-        staleTime: 60 * 1000, // 60s
-        retry: 3
-    });
+    const { data: users, error, isLoading } = useUsers();
 
     if(isLoading) return <Skeleton />
 
     if(error) return null;
 
+    const assignIssue = (userId: string) => {
+
+        /** We don't need to await this call because we don't need to do anything after
+         * The select will display the selected user and the patch request can take it's time
+         * to register in the database
+         */
+
+        // Send the assigned user id if it exists or null if we are unassigning
+
+        axios.patch("/api/issues/" + issue.id, { assignedToUserId: userId || null })
+        .catch(() => {
+            toast.error("Changes could not be saved");
+        });
+    }
+
     return (
         <>
-            <Select.Root defaultValue={ issue.assignedToUserId || "" } onValueChange={(userId) => {
-
-                /** We don't need to await this call because we don't need to do anything after
-                 * The select will display the selected user and the patch request can take it's time
-                 * to register in the database
-                 */
-
-                // Send the assigned user id if it exists or null if we are unassigning
-
-                axios.patch("/api/issues/" + issue.id, { assignedToUserId: userId || null })
-                .catch(() => {
-                    toast.error("Changes could not be saved");
-                });
-            }}>
+            <Select.Root defaultValue={ issue.assignedToUserId || "" } onValueChange={ assignIssue }>
                 <Select.Trigger placeholder="Assign"/>
                     <Select.Content>
                         <Select.Group>
@@ -57,5 +54,14 @@ const AssigneeSelect = ({ issue }: Props) => {
         </>
     )
 }
+
+// Extract the request and caching to a hook
+
+const useUsers = () => useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then(res => res.data),
+    staleTime: 60 * 1000, // 60s
+    retry: 3
+});
 
 export default AssigneeSelect
